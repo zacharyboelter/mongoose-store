3f6c3acf0f8cb0e55b594ef7b859515949fe7458
Zachary Boelter <zboelter@gmail.com>
2021-09-29 17:52:23 -0500

    css

--- server.js
+++ server.js
@@ -35,35 +35,35 @@ app.use(express.static('public'));
 // ===========================================================
 // SEED DATA 
 // ===========================================================
-// app.get('/products/seed', (req, res) => {
-//     Product.create(
-//         [
-//             {
-//                 name: 'Beans',
-//                 description: 'A small pile of beans. Buy more beans for a big pile of beans.',
-//                 img: 'https://imgur.com/LEHS8h3.png',
-//                 price: 5,
-//                 qty: 99
-//             }, {
-//                 name: 'Bones',
-//                 description: 'It\'s just a bag of bones.',
-//                 img: 'https://imgur.com/dalOqwk.png',
-//                 price: 25,
-//                 qty: 0
-//             }, {
-//                 name: 'Bins',
-//                 description: 'A stack of colorful bins for your beans and bones.',
-//                 img: 'https://imgur.com/ptWDPO1.png',
-//                 price: 7000,
-//                 qty: 1
-//             }
-//         ]
-//         ,
-//         (error, data) => {
-//             res.redirect('/products');
-//         }
-//     );
-// });
+app.get('/products/seed', (req, res) => {
+    Product.create(
+        [
+            {
+                name: 'Beans',
+                description: 'A small pile of beans. Buy more beans for a big pile of beans.',
+                img: 'https://imgur.com/LEHS8h3.png',
+                price: 5,
+                qty: 99
+            }, {
+                name: 'Bones',
+                description: 'It\'s just a bag of bones.',
+                img: 'https://imgur.com/dalOqwk.png',
+                price: 25,
+                qty: 0
+            }, {
+                name: 'Bins',
+                description: 'A stack of colorful bins for your beans and bones.',
+                img: 'https://imgur.com/ptWDPO1.png',
+                price: 7000,
+                qty: 1
+            }
+        ]
+        ,
+        (error, data) => {
+            res.redirect('/products');
+        }
+    );
+});
 // ===========================================================
 // ROUTES 
 // ===========================================================

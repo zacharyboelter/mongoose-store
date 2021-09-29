198dcf8f9df3d3bcaa3be5f001dd7913e6246ce0
Zachary Boelter <zboelter@gmail.com>
2021-09-29 10:49:42 -0500

    delete route/link

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
 const Product = require('./models/products.js');
+const methodOverride = require('method-override');
 
 // ===========================================================
 // MIDDLEWARE
@@ -14,7 +15,7 @@ const Product = require('./models/products.js');
 
 //Body parser middleware: give us access to req.body
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride('_method'));
 // Database Connection
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
@@ -34,35 +35,35 @@ app.use(express.static('public'));
 // ===========================================================
 // SEED DATA 
 // ===========================================================
-app.get('/products/seed', (req, res) => {
-    Product.create(
-        [
-            {
-                name: 'Beans',
-                description: 'A small pile of beans. Buy more beans for a big pile of beans.',
-                img: 'https://imgur.com/LEHS8h3.png',
-                price: 5,
-                qty: 99
-            }, {
-                name: 'Bones',
-                description: 'It\'s just a bag of bones.',
-                img: 'https://imgur.com/dalOqwk.png',
-                price: 25,
-                qty: 0
-            }, {
-                name: 'Bins',
-                description: 'A stack of colorful bins for your beans and bones.',
-                img: 'https://imgur.com/ptWDPO1.png',
-                price: 7000,
-                qty: 1
-            }
-        ]
-        ,
-        (error, data) => {
-            res.redirect('/products');
-        }
-    );
-});
+// app.get('/products/seed', (req, res) => {
+//     Product.create(
+//         [
+//             {
+//                 name: 'Beans',
+//                 description: 'A small pile of beans. Buy more beans for a big pile of beans.',
+//                 img: 'https://imgur.com/LEHS8h3.png',
+//                 price: 5,
+//                 qty: 99
+//             }, {
+//                 name: 'Bones',
+//                 description: 'It\'s just a bag of bones.',
+//                 img: 'https://imgur.com/dalOqwk.png',
+//                 price: 25,
+//                 qty: 0
+//             }, {
+//                 name: 'Bins',
+//                 description: 'A stack of colorful bins for your beans and bones.',
+//                 img: 'https://imgur.com/ptWDPO1.png',
+//                 price: 7000,
+//                 qty: 1
+//             }
+//         ]
+//         ,
+//         (error, data) => {
+//             res.redirect('/products');
+//         }
+//     );
+// });
 // ===========================================================
 // ROUTES 
 // ===========================================================
@@ -80,7 +81,7 @@ app.get('/products', (req, res) => {
 });
 
 // ===========================================================
-// NEW - display form to add a new book
+// NEW - display form to add a new product
 // ===========================================================
 app.get('/products/new', (req, res) => {
     res.render('new.ejs');
@@ -89,19 +90,24 @@ app.get('/products/new', (req, res) => {
 
 
 // ===========================================================
-// DELETE - delete a single book
+// DELETE - delete a single product
+// ===========================================================
+app.delete('/products/:id', (req, res) => {
+    Product.findByIdAndDelete(req.params.id, (err, data) => {
+        res.redirect('/products');
+    });
+});
+
+
+
+// ===========================================================
+// UPDATE - update a single product
 // ===========================================================
 
 
 
 // ===========================================================
-// UPDATE - update a single book
-// ===========================================================
-
-
-
-// ===========================================================
-// CREATE - create a new book
+// CREATE - create a new product
 // ===========================================================
 app.post('/products', (req, res) => {
     Product.create(req.body, (error, createdProduct) => {
@@ -109,13 +115,13 @@ app.post('/products', (req, res) => {
     });
 });
 // ===========================================================
-// EDIT - display form to update a book
+// EDIT - display form to update a product
 // ===========================================================
 
 
 
 // ===========================================================
-// SHOW - display a single book
+// SHOW - display a single product
 // ===========================================================
 
 app.get('/products/:id', (req, res) => {
